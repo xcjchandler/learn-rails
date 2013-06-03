1980748eb2cc84dd625d95365aaa7ffd2c13111b
xcjchandler <xcjchandler@gmail.com>
2013-06-03 17:12:07 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChandlerLearnRails::Application.config.secret_token = '077e204842f55cce12e751931b53721756ed214431501b17460d21db4754012a8efb043c4d9674469d476bd2fe186df05958ab63f72add1b0b1b7eec176cb5d2'
